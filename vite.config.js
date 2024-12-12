a9edd0c490faaf769c53ee0d88d75dc5a26dabbf
johanesrendy <johanesrendy9@gmail.com>
2024-12-12 23:58:57 +0700

    chore: reconfigure vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,18 +9,12 @@ export default defineConfig({
         }),
     ],
     server: {
-        // Agar bisa diakses dari semua IP, bukan hanya localhost
-        host: "0.0.0.0", // Membuat Vite dapat diakses dari semua IP
-        port: 5173, // Gunakan port yang sesuai, default adalah 5173
+        host: "0.0.0.0",
+        port: 5173,
         cors: {
-            // Mengizinkan semua origin untuk mengakses resource
-            origin: "*", // Kamu bisa mengganti '*' dengan domain tertentu jika perlu
+            origin: "*",
             methods: ["GET", "POST", "PUT", "DELETE"],
             allowedHeaders: ["Content-Type", "Authorization"],
-        },
-        proxy: {
-            // Proksi untuk API Laravel jika diperlukan
-            "/api": "http://localhost", // Ubah '/api' sesuai dengan path API Laravel
         },
     },
 });
