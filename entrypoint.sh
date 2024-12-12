b170d981895d7eb66b05fc03142b3ba52137cacd
johanesrendy <johanesrendy9@gmail.com>
2024-12-12 23:07:53 +0700

    chore: add entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Bersihkan cache Laravel
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+
+# Cek apakah ada file package.json dan jalankan npm install dan build secara berurutan
+if [ -f package.json ]; then
+    echo "Running npm dev server..."
+    npm run dev & # Menjalankan npm run dev di latar belakang (untuk development server)
+fi
+
+# Jalankan PHP-FPM
+php-fpm
