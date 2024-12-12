91f9ec85d33e62e5742d535b9ae6f0b35eb30dc1
johanesrendy <johanesrendy9@gmail.com>
2024-12-12 23:30:34 +0700

    chore: add entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,11 @@ php artisan route:clear
 # Cek apakah ada file package.json dan jalankan npm install dan build secara berurutan
 if [ -f package.json ]; then
     echo "Running npm dev server..."
-    npm run dev & # Menjalankan npm run dev di latar belakang (untuk development server)
+    npm run dev &  # Menjalankan npm run dev di latar belakang (untuk development server)
 fi
 
 # Jalankan PHP-FPM
 php-fpm
+
+# Menunggu semua background processes agar tidak keluar saat container mati
+wait
