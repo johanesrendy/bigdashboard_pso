0562376662bcf168acc72c82ccab322eda5262d8
johanesrendy <johanesrendy9@gmail.com>
2024-12-13 00:06:45 +0700

    chore: reconfigure vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     server: {
         host: "0.0.0.0",
         port: 5173,
+        https: false,
         cors: {
             origin: "*",
             methods: ["GET", "POST", "PUT", "DELETE"],
