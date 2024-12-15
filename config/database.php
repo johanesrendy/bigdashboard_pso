7ab15501ee5fab2c7bec726de79106a04b15b0f5
johanesrendy <johanesrendy9@gmail.com>
2024-12-16 03:30:47 +0700

    `Update database configuration with new host, username, and password.`

--- config/database.php
+++ config/database.php
@@ -45,11 +45,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '3.106.237.6'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'username' => env('DB_USERNAME', 'phpmyadmin'),
+            'password' => env('DB_PASSWORD', 'phpmyadmin'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
@@ -59,8 +59,10 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::ATTR_TIMEOUT => 900,
             ]) : [],
         ],
+
 
         'mariadb' => [
             'driver' => 'mariadb',
